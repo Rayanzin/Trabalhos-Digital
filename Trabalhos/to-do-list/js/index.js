56fae9362b4e60836a0dafb97c0d8a997b79b2ec
Rayan <139246019+Rayanzin@users.noreply.github.com>
2024-07-30 18:14:19 -0300

    confirmar exclusão e query ok

--- Trabalhos/to-do-list/js/index.js
+++ Trabalhos/to-do-list/js/index.js
@@ -1,14 +1,31 @@
 let modalenter = document.querySelector(".modal")
-function novaTarefa(){
+function novaTarefa() {
     overlay.classList.add("active");
-        modalenter.classList.add("modalon")
+    modalenter.classList.add("modalon")
 }
-function fecharModal(){
-    overlay.classList.remove("active"); 
+function fecharModal() {
+    overlay.classList.remove("active");
     modalenter.classList.remove("modalon")
+    cancelar.classList.remove("active");
 }
 document.addEventListener('keyup', (e) => {
-    if(e.key == "Escape"){
+    if (e.key == "Escape") {
         fecharModal()
     }
 })
+function desejaExcluir() {
+    overlay.classList.add("active");
+    cancelar.classList.add("active");
+}
+
+let div
+lista.addEventListener('click', function (event) {
+    if (event.target.classList.contains('btnExcluir')) {
+        let deletar = event.target.parentElement;
+        div = deletar.parentNode
+    }
+})
+function excluirTarefa() {
+    lista.removeChild(div);
+    fecharModal()
+}
